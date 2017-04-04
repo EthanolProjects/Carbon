8fa426600ecaf155831648b5e915d6bc852e7b0f
DREAMWORLDVOID <yshliu0321@icloud.com>
2017-04-04 22:44:50 +0800

    Log Change Definition 2

--- Carbon/Log.hpp
+++ Carbon/Log.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <sstream>
 namespace Carbon {
     class Logger{
@@ -11,19 +12,23 @@ namespace Carbon {
         // Log Entry and Log Function
         class Entry {
         public:
-            Entry(const char* func, const char* file, int line, Logger& hst);
+            Entry(const char* func, const char* file, std::int32_t line, std::int32_t sev, Logger& hst);
             ~Entry();
             template <class T>
             Entry& operator << (T&& rhs) { mStringBuf << rhs; return *this; }
         private:
             Logger& mHost;
+            const char* mFunc;
+            const char* mFile;
+            std::int32_t mLine, mSev;
             std::stringstream mStreamBuf;
         };
         template <class Sev>
-        auto operator()(const char* file, int line, Sev sev = Sev()) { return logSev(static_cast<unsigned long>(sev)); }
+        auto operator()(const char* func, const char* file, std::int32_t line, Sev sev = Sev()) {
+            return Entry(func, file, line, static_cast<std::int32_t>(sev));
+        }
     private:
-        void recordEntry(const char* func, const char* file, int line, std::stringstream& buffer);
-        Entry logSev(const char* func, const char* file, int line, unsigned long sevLev);
+        void recordEntry(const char* func, const char* file, std::int32_t line, std::int32_t sev, std::stringstream& buffer);
     };
 #define CARBON_LOG_SEV(logger, sev) logger(__FUNC__, __FILE__, __LINE__, sev)
 }
