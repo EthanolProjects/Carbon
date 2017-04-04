fe93cfe445258dd529a4a29f5d7787f1dc56c93f
DREAMWORLDVOID <yshliu0321@icloud.com>
2017-04-04 21:42:30 +0800

    Log Change Definition

--- Carbon/Log.hpp
+++ Carbon/Log.hpp
@@ -1,17 +1,29 @@
-#include <../details/LoggerDetail.hpp>
-
-namespace Carbon
-{
-	template<typename Level>
-	class Logger:Uncopyable
-	{
-	private:
-		const std::function<void(Level,const std::stringstream&)> m_flush;
-	public:
-		Logger(const std::function<void(Level,std::stringstream&)> flush):m_flush(flush){}
-		auto operator()(Level level)
-		{
-			return detail::Message<Level>(level);
-		}
-	}
+#pragma once
+#include <sstream>
+namespace Carbon {
+    class Logger{
+    public:
+        // Default Constructable
+        Logger() = default;
+        // Non-Copyable
+        Logger(const Logger&) = delete;
+        Logger& operator = (const Logger&) = delete;
+        // Log Entry and Log Function
+        class Entry {
+        public:
+            Entry(const char* func, const char* file, int line, Logger& hst);
+            ~Entry();
+            template <class T>
+            Entry& operator << (T&& rhs) { mStringBuf << rhs; return *this; }
+        private:
+            Logger& mHost;
+            std::stringstream mStreamBuf;
+        };
+        template <class Sev>
+        auto operator()(const char* file, int line, Sev sev = Sev()) { return logSev(static_cast<unsigned long>(sev)); }
+    private:
+        void recordEntry(const char* func, const char* file, int line, std::stringstream& buffer);
+        Entry logSev(const char* func, const char* file, int line, unsigned long sevLev);
+    };
+#define CARBON_LOG_SEV(logger, sev) logger(__FUNC__, __FILE__, __LINE__, sev)
 }
--- details/common.hpp
+++
@@ -1,8 +0,0 @@
-struct Uncopyable
-{
-    Uncopyable() = default;
-    Uncopyable(const Uncopyable&) = delete;
-    Uncopyable& operator=(const Uncopyable&) = delete;
-    Uncopyable(Uncopyable&&) = default;
-    Uncopyable& operator=(Uncopyable&&) = default;
-};
