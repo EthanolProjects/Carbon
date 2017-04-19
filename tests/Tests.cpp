a5b583ecd13bc87c36df3805dec3bc79dfa9f2dc
DREAMWORLDVOID <yshliu0321@icloud.com>
2017-04-19 17:17:52 +0800

    0

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,97 +1,54 @@
+#include UNITTESTINC
 #include "Concurrency.hpp"
 #include "Log.hpp"
-#include <iostream>
 #include <vector>
-#include <chrono>
 #include <future>
 
-using namespace std::chrono;
-using namespace std::chrono_literals;
-using cclock = steady_clock;
+namespace CarbonTests {
+    namespace ConcurrencyTests {
+        constexpr size_t maxTestCount = 10000000;
+        auto obj = []() {
+            int c = 0;
+            for (int i = 0; i < 100; ++i)
+                c += rand();
+            return c;
+        };
 
-constexpr size_t maxTestCount = 10000000;
-size_t testCount = 1;
+        BEGIN_TEST_GROUP(Concurrency)
+            size_t testCount = 1;
+        void test() {
+            std::vector<std::future<int>> futs; futs.reserve(testCount);
+            std::vector<int> result(testCount);
+            for (size_t i = 0; i < testCount; ++i)
+                result[i] = obj();
+        }
 
-class MyLogger :public Carbon::Logger {
-private:
-    std::string format(const char* func , const char* file , std::int32_t line , std::int32_t sev , 
-        const std::string& str) const override {
-        return "Logger :"+str;
-    }
-    void flush(const std::string& buffer) override {
-        std::cout << buffer<<std::endl;
-    }
-} logger;
-auto obj = [] () {
-    int c = 0;
-    for (int i = 0; i < 100; ++i)
-        c += rand();
-    return c;
-};
-
-auto test() {
-    std::vector<std::future<int>> futs; futs.reserve(testCount);
-    std::vector<int> result(testCount);
-    auto start = cclock::now();
-    for (size_t i = 0; i < testCount; ++i)
-        result[i] = obj();
-    return (cclock::now() - start) / 1us;
-}
-
-auto testOMP() {
-    std::vector<std::future<int>> futs; futs.reserve(testCount);
-    std::vector<int> result(testCount);
-    auto start = cclock::now();
-
+        void testOMP() {
+            std::vector<std::future<int>> futs; futs.reserve(testCount);
+            std::vector<int> result(testCount);
 #pragma omp parallel for
-    for (int i = 0; i < testCount; ++i)
-        result[i] = obj();
-    return (cclock::now() - start) / 1us;
-}
+            for (int i = 0; i < testCount; ++i)
+                result[i] = obj();
+        }
 
-auto testThreadPool() {
+        void testThreadPool() {
+            Carbon::ThreadPool pool{};
+            std::vector<std::future<int>> futs; futs.reserve(testCount);
+            for (size_t i = 0; i < testCount; ++i)
+                futs.push_back(Carbon::Async(pool, obj));
+            for (auto&& a : futs)
+                a.wait();
+        }
 
-    Carbon::ThreadPool pool {};
-    std::vector<std::future<int>> futs; futs.reserve(testCount);
-    auto start = cclock::now();
-    for (size_t i = 0; i < testCount; ++i)
-        futs.push_back(Carbon::Async(pool , obj));
-    for (auto&& a : futs)
-        a.wait();
-    return (cclock::now() - start) / 1us;
-}
-
-auto testThreadPoolTaskGroup() {
-
-    Carbon::ThreadPool pool {};
-    std::vector<int> result(testCount);
-    auto start = cclock::now();
-    using Range = Carbon::TaskGroupHelper::IntegerRange;
-    std::function<void(Range)> func = Range::forEach([&] (size_t i) {result[i] = obj(); });
-    auto future = Carbon::AsyncGroup(pool , func ,
-        Carbon::TaskGroupHelper::IntegerRange { 0,testCount });
-    future->wait();
-    return (cclock::now() - start) / 1us;
-}
-
-int main() {
-    while (testCount<=maxTestCount) {
-        std::cout << "Async Benchmark for Small Tasks:" << testCount << "ops" << std::endl;
-        float t0;
-        long long t;
-        t0 = t = test();
-        t0 *= 100.0f;
-        std::cout.precision(2);
-        std::cout << std::fixed;
-        std::cout << "Default:" << t << "us " << 100.0f << "%" << std::endl;
-        t = testOMP();
-        std::cout << "OpenMP:" << t << "us " << t0 / t << "%" << std::endl;
-        t = testThreadPool();
-        std::cout << "ThreadPool:" << t << "us " << t0 / t << "%" << std::endl;
-        t = testThreadPoolTaskGroup();
-        std::cout << "ThreadPool with TaskGroup:" << t << "us " << t0 / t << "%" << std::endl;
-        testCount *= 10;
+        void testThreadPoolTaskGroup() {
+            Carbon::ThreadPool pool{};
+            std::vector<int> result(testCount);
+            using Range = Carbon::TaskGroupHelper::IntegerRange;
+            std::function<void(Range)> func = Range::forEach([&](size_t i) {result[i] = obj(); });
+            auto future = Carbon::AsyncGroup(pool, func,
+                Carbon::TaskGroupHelper::IntegerRange{ 0,testCount });
+            future->wait();
+        }
+        END_TEST_GROUP
     }
-    system("pause");
-    return 0;
 }
