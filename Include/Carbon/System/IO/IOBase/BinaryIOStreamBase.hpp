e03051856d464e046035a9de3570b09592945489
予顺 刘 <yshliu0321@icloud.com>
2017-06-11 22:24:41 +0800

    Modifications

--- Include/Carbon/System/IO/IOBase/BinaryIOStreamBase.hpp
+++ Include/Carbon/System/IO/IOBase/BinaryIOStreamBase.hpp
@@ -10,13 +10,13 @@ namespace Carbon {
 
             struct IInputStream : virtual StreamBase {
                 virtual Byte get() const noexcept = 0;
-                virtual void read(Byte* targetBuffer, long long readLengthByBytes) const noexcept = 0;
-                virtual int readSome(const Byte* targetBuffer, long long readLengthByBytes) const noexcept = 0;
+                virtual void read(Byte* targetBuffer, long long readLengthByBytes) const = 0;
+                virtual int readSome(Byte* targetBuffer, long long readLengthByBytes) const noexcept = 0;
             };
 
             struct IOutputStream : virtual StreamBase {
                 virtual Byte put() const noexcept = 0;
-                virtual void write(const Byte* targetBuffer, long long writeLengthByBytes) noexcept = 0;
+                virtual void write(const Byte* targetBuffer, long long writeLengthByBytes) = 0;
                 virtual int writeSome(const Byte* targetBuffer, long long writeLengthByBytes) noexcept = 0;
             };
 
@@ -38,13 +38,39 @@ namespace Carbon {
                 StreamBase& mStream;
             };
 
-            class RawIStreamView : StreamViewBase {
+            class RawIStreamView : virtual protected StreamViewBase {
             public:
                 RawIStreamView(IInputStream& stream) : StreamViewBase(stream) {}
-                void read(Byte* targetBuffer, long long readLengthByBytes) const noexcept { getStream<IInputStream>().read(targetBuffer, readLengthByBytes); }
-                int readSome(const Byte* targetBuffer, long long readLengthByBytes) const noexcept { getStream<IInputStream>().readSome(targetBuffer, readLengthByBytes); }
+                void read(Byte* targetBuffer, long long readLengthByBytes) const { getStream<IInputStream>().read(targetBuffer, readLengthByBytes); }
+                int readSome(Byte* targetBuffer, long long readLengthByBytes) const noexcept { getStream<IInputStream>().readSome(targetBuffer, readLengthByBytes); }
                 template <class T>
-                RawIStreamView& operator >> (T& in) { getStream<IInputStream>().read(&in, sizeof(T)); return *this; }
+                RawIStreamView& operator >> (T& in) const { getStream<IInputStream>().read(&in, sizeof(T)); return *this; }
+            };
+
+            class RawOStreamView : virtual protected StreamViewBase {
+            public:
+                RawOStreamView(IOutputStream& stream) : StreamViewBase(stream) {}
+                void write(const Byte* targetBuffer, long long writeLengthByBytes) { getStream<IOutputStream>().write(targetBuffer, writeLengthByBytes); }
+                int writeSome(const Byte* targetBuffer, long long writeLengthByBytes) noexcept { getStream<IOutputStream>().writeSome(targetBuffer, writeLengthByBytes); }
+                template <class T>
+                RawOStreamView& operator << (const T& in) { getStream<IOutputStream>().write(&in, sizeof(T)); return *this; }
+            };
+
+            class RawIOStreamView : public RawIStreamView, public RawOStreamView {
+            public:
+                RawIOStreamView(IIOStream& stream) : RawIStreamView(stream), RawOStreamView(stream), StreamViewBase(stream) {}
+            };
+
+            struct RAMBase {};
+
+            struct IReadOnlyRAM {
+                virtual Byte getAt(unsigned long long at) const = 0;
+                virtual void readAt(Byte* targetBuffer, unsigned long long at, unsigned long long size) const = 0;
+            };
+
+            struct IWriteOnlyRAM {
+                virtual void putAt(const Byte in, unsigned long long at) = 0;
+                virtual void writeAt(const Byte* fromBuffer, unsigned long long at, unsigned long long size) = 0;
             };
 
         }
